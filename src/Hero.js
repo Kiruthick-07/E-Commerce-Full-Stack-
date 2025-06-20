cc065d8bce1bacc42863b6fb27c333d89bf28c81
Kiruthick-07 <tamilelectroniv@gmail.com>
2025-06-20 10:25:30 +0530

    second commit : hero-section done

--- src/Hero.js
+++ src/Hero.js
@@ -4,11 +4,17 @@ import './Hero.css'
 const Hero = () => {
   return (
     <>
-    <div className='hero'>
-      <h1>Effortless fashion for everyday,</h1>
-      <h4>Designed with style , Worn with pride.</h4>
-      <img src='model bg removed.png'></img>
-    </div>
+      <div className="hero-container">
+        <div className='overlay'></div>
+        <img className='heroimg' src='parker-burchfield-tvG4WvjgsEY-unsplash-removebg-preview.png'></img>
+        <div className='hero-text'>
+        <h1>From Basic to Bold – We've Got You.</h1>
+        <p>Dress with intent. Style with purpose</p>
+        <button className='shopnow'>Shop Now</button>
+        <button className='explore'>Explore</button>
+        </div>
+      </div>
+
     </>
   )
 }
