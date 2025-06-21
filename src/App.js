7f918386333e2ec5d753975af10c9e7837d43a4e
Kiruthick-07 <tamilelectroniv@gmail.com>
2025-06-21 13:42:39 +0530

    product component initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './Hero';
 import Header from './Header';
 import Category from './Category';
+import Products from './Products';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Category />
+      <Products />
     </div>
   );
 }
